eebaa63db20c716ed3c9ed55129903922f43f4d4
Max <135263966+verifizieren@users.noreply.github.com>
2023-06-09 00:57:11 +0200

    Update github-updater.py

--- github-updater.py
+++ github-updater.py
@@ -1,35 +1,58 @@
-import requests
-from time import sleep
+import asyncio
+import aiohttp
+import sys
 
-welcome_art = """
+WELCOME_ART = """
  _____ _ _   _           _       _   _ _                        
 |  __ (_| | | |         | |     | | | (_)                       
 | |  \/_| |_| |__  _   _| |__   | | | |_  _____      _____ _ __ 
 | | __| | __| '_ \| | | | '_ \  | | | | |/ _ \ \ /\ / / _ | '__|
 | |_\ | | |_| | | | |_| | |_) | \ \_/ | |  __/\ V  V |  __| |   
- \____|_|\__|_| |_|\__,_|_.__/   \___/|_|\___| \_/\_/ \___|_|                                                            
+ \____|_|\__|_| |_|\__,_|_.__/   \___/|_|\___| \_/\_/ \___|_|                                                                  
 """
 
-def requester(url):
-    re = requests
-    req = re.get(url=url) # Requesting given URL
-    return req.status_code
+async def send_request(session, url):
+    """Asynchronously send a GET request to a URL."""
+    async with session.get(url) as response:
+        if response.status == 200:
+            print("[+] Successful")
 
-class github_update:
-    print(welcome_art + "\n") # Prints the welcome art
-    target = input(f"[*] Please input your viewcount link: ") # Getting the target URL
-    req_amount = int(input(f"[*] Please input number of requets: ")) # Getting Request amount
 
-    for i in range(req_amount):
-        req = requester(target)
-        if req == 200:  # Checking status code
-            print("Succesfull")
-        else:
-            print("Failed")
-        #sleep(1)
+class GithubUpdate:
+    """Class for GitHub Update."""
+    def __init__(self):
+        print(WELCOME_ART + "\n")
+        self.target = input("[*] Please input your viewcount link: ")
+        self.req_num = int(input("[*] Please input number of requests: "))
+        self.delay = float(input("[*] Please input delay (in milliseconds): "))
+        self.loop = asyncio.get_event_loop()
+
+    async def start(self):
+        """Start sending requests asynchronously."""
+        tasks = [send_request(aiohttp.ClientSession(), self.target) for _ in range(self.req_num)]
+        await asyncio.gather(*tasks)
+
+    def run(self):
+        """Execute the start method."""
+        self.loop.run_until_complete(self.start())
+
+    def stop(self):
+        """Stop all running tasks and exit the program."""
+        print("[*] Stopping...")
+        for task in asyncio.Task.all_tasks():
+            task.cancel()
+        sys.exit(0)
+
 
 def main():
-    github_update()
+    """Main function."""
+    updater = GithubUpdate()
+    updater.run()
+
+    while True:
+        if input() == 'S':
+            updater.stop()
+
 
 if __name__ == "__main__":
     main()
